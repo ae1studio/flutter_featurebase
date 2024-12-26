9de81fe1b7ca42bb59a36068e3d6d4db93f790bb
Elias <elias@deuss.dev>
2024-12-26 12:54:39 -0800

    fix(example): hide debug banner

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Featurebase Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromARGB(255, 80, 72, 230),
